794cb9497aa69975c0e1f77caa8066912fc1fe02
jrwspina <jrwspina@gmail.com>
2026-03-24 17:35:11 -0300

    feat: postgresql setup with docker and settings

---
+++ app/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from settings import settings
+
+engine = create_async_engine(
+    f"postgresql+asyncpg://{settings.postgres_user}:{settings.postgres_password}@localhost/{settings.postgres_db}"
+)
+async_session = async_sessionmaker(engine)
+
+
+async def get_db():
+    async with async_session() as session:
+        yield session
