b1a46b6c0f6395b386dd17e68b8bff4ff0f1a522
jrwspina <jrwspina@gmail.com>
2026-05-02 21:14:22 -0300

    feat: add test fixtures for auth and database setup

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -8,6 +8,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 
 from app.database import get_db
 from app.main import app
+from app.models import User
+from app.security import get_password_hash
 from app.settings import settings
 
 engine = create_async_engine(f"{settings.test_postgres_url}")
@@ -40,3 +42,43 @@ def override_db():
 async def client(override_db):
     async with AsyncClient(app=app, base_url="http://test") as ac:
         yield ac
+
+
+@pytest.fixture
+async def session():
+    async with async_session() as session:
+        yield session
+
+
+@pytest.fixture
+async def sample_user(session):
+    user = User(
+        firstname="name",
+        lastname="lastname",
+        email="test@test.com",
+        password_hash=get_password_hash("password"),
+    )
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    yield user
+    await session.delete(user)
+    await session.commit()
+
+
+@pytest.fixture
+async def auth_token(client, sample_user):
+    response = await client.post(
+        "auth/token",
+        data={
+            "username": sample_user.email,
+            "password": "password",
+        },
+    )
+
+    return response.json()["access_token"]
+
+
+@pytest.fixture
+async def auth_headers(auth_token):
+    return {"Authorization": f"Bearer {auth_token}"}
